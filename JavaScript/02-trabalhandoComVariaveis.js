e1db23ff91fc01d8a08309c7d9a84b10d690e12d
Walison Fernandes <walisonfbalves.ti@gmail.com>
2021-05-17 09:59:19 -0300

    update

---
+++ JavaScript/02-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+console.log('Trabalhando com Variáveis');
+// JS é case sensitive
+
+const idade = 29;  
+const nome = 'Ricardo';
+
+console.log(idade, nome);
---
+++ JavaScript/05-atribuicaovariaveis.js
@@ -0,0 +1,16 @@
+console.log('Trabalhando com Variáveis');
+const idade = 29;  
+let nome = 'Ricardo';
+let sobrenome = "Dutra"
+
+// console.log(nome + " " + sobrenome);
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nome = nome + sobrenome;
+console.log(nome)
+const nomeCompleto = nome;
+console.log(nomeCompleto);
+
+
